51e27a55e931ad86548422e86eac2109d32ee4f0
Conrad <conrad_bezuidenhout@mckinsey.com>
2021-10-21 16:40:12 +0800

    init

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 import streamlit as st
 from auth0_component import login_button
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
 clientId = st.secrets['auth']['clientId']
 domain = st.secrets['auth']['domain']
